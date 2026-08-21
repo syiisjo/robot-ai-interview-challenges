8b510f5520d447db70e00b5cc7c08b6a801b98d9
syiisjo <wxy2063992759@qq.com>
2026-08-21 14:15:23 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
 
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass(frozen=True)
+class Event:
+    event_type: str
+    timestamp: float
+    person_id: Optional[str] = None
+
+@dataclass(frozen=True)
+class Effect:
+    effect_type: str
+    value: str
+    reason: str
